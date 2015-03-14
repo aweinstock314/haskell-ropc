c4e3186d92f5d21678d2755e393f3dc9b46c3f1a
Avi Weinstock <aweinstock314@gmail.com>
2015-03-14 19:40:28 -0400

    Added gadget search and display functions.

--- test_section_getter.hs
+++ test_section_getter.hs
@@ -1,10 +1,16 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
+--module ROPc where
+import Control.Arrow
+import Control.Monad.Identity
+import Data.List
 import Data.Char
+import Data.Maybe
 import Data.Word
 import Foreign.C
 import Foreign.Marshal
 import Foreign.Ptr
 import Foreign.Storable
+import Harpy.X86Disassembler
 import Numeric
 import qualified Data.Vector as V
 
@@ -22,7 +28,20 @@ getSectionByName fname section =
         free result
         return vec
 
+eitherToMaybe = either (const Nothing) Just
+
+getGadgets backLength codeVec = nub . concat $ map tryPosition (V.toList retIdxs) where
+    retIdxs = V.findIndices (== 0xc3) codeVec
+    len = V.length codeVec
+    invalidSlice i j = (i+j) > len || i < 0
+    trySlice :: Int -> Int -> Maybe [Instruction]
+    trySlice i j | invalidSlice i j = Nothing
+    trySlice i j = eitherToMaybe . runIdentity . disassembleList . V.toList $ V.slice i j codeVec
+    tryPosition j = catMaybes $ map (\i -> fmap ((,)i) $ trySlice (j-i) (i+1)) [0..backLength]
+
 main = do
     textSection <- getSectionByName "./a.out" ".text"
     V.mapM_ (putStr . flip showHex "" . fromIntegral) textSection
     putStrLn ""
+    let gadgets = getGadgets 5 textSection
+    putStr . unlines $ map (show . second show) gadgets
