aecae5918a03bd080c9f537d8af07c1e51226c9c
Avi Weinstock <aweinstock314@gmail.com>
2015-03-15 11:38:49 -0400

    Gadgets are now checked to make sure they contain valid instructions. Some arguments are now taken via argv.

--- test_section_getter.hs
+++ test_section_getter.hs
@@ -12,6 +12,7 @@ import Foreign.Ptr
 import Foreign.Storable
 import Harpy.X86Disassembler
 import Numeric
+import System.Environment
 import qualified Data.Vector as V
 
 foreign import ccall "get_section_by_name"
@@ -30,18 +31,36 @@ getSectionByName fname section =
 
 eitherToMaybe = either (const Nothing) Just
 
-getGadgets backLength codeVec = nub . concat $ map tryPosition (V.toList retIdxs) where
-    retIdxs = V.findIndices (== 0xc3) codeVec
+getGadgets backLength codeVec = nub . validate . concat $ map tryPosition retIdxs where
+    retIdxs = V.toList $ V.findIndices (== 0xc3) codeVec
     len = V.length codeVec
     invalidSlice i j = (i+j) > len || i < 0
     trySlice :: Int -> Int -> Maybe [Instruction]
     trySlice i j | invalidSlice i j = Nothing
     trySlice i j = eitherToMaybe . runIdentity . disassembleList . V.toList $ V.slice i j codeVec
-    tryPosition j = catMaybes $ map (\i -> fmap ((,)i) $ trySlice (j-i) (i+1)) [0..backLength]
+    tryPosition j = catMaybes $ map (\i -> fmap ((,)(j-i)) $ trySlice (j-i) (i+1)) [0..backLength]
+    isValid (Instruction op _ _ _ _) = show op /= "InvalidOpcode"
+    isValid _ = False
+    validate = filter (all isValid . snd)
+
+
+showHexList lst = "[" ++ concat (intersperse "," (map (flip showHex "") lst)) ++ "]"
+
+myShowInstruction (Instruction op _ args _ bytes) = show op ++ showHexList bytes
+myShowInstruction (BadInstruction a b c d) = "???" ++ show (a,b,c,d)
+myShowInstruction (PseudoInstruction a b) = "???" ++ show (a,b)
 
 main = do
-    textSection <- getSectionByName "./a.out" ".text"
+    args <- getArgs
+    case args of
+        [] -> main' "./a.out" "5" ".text"
+        [fname] -> main' fname "5" ".text"
+        [fname, backLen] -> main' fname backLen ".text"
+        [fname, backLen, section] -> main' fname backLen section
+
+main' fname numGadgets section = do
+    textSection <- getSectionByName fname section
     V.mapM_ (putStr . flip showHex "" . fromIntegral) textSection
     putStrLn ""
-    let gadgets = getGadgets 5 textSection
-    putStr . unlines $ map (show . second show) gadgets
+    let gadgets = getGadgets (read numGadgets) textSection
+    putStr . unlines $ map (show . second (map myShowInstruction)) gadgets
